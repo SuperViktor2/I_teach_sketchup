a3f652ed834aafc252cdbec1b0f735b8165b0431
SuperViktor2 <44811126+SuperViktor2@users.noreply.github.com>
2022-06-02 20:54:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function about () {
+	var element = document.querySelector("#about");
+	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function courses () {
+	var element = document.querySelector("#program");
+	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+
+function apps () {
+	var element = document.querySelector("#apps");
+	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+}

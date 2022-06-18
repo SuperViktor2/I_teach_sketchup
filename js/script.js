0ddd1d0d01d6cf253772f8398861f87b2b0edc4f
SuperViktor2 <44811126+SuperViktor2@users.noreply.github.com>
2022-06-18 22:30:47 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-function about () {
-	var element = document.querySelector("#about");
-	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
-}
-
-function courses () {
-	var element = document.querySelector("#program");
-	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
-}
-
-function apps () {
-	var element = document.querySelector("#apps");
-	element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+function showCourse(evt, course) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(course).style.display = "block";
+  evt.currentTarget.className += " active";
 }
